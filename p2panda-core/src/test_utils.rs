3ca4ef76ca7b0bb27bda57da1539131935857d7a
Matthias Beyer <mail@beyermatthias.de>
2026-08-19 11:36:04 +0200

    WIP

--- p2panda-core/src/test_utils.rs
+++ p2panda-core/src/test_utils.rs
@@ -40,109 +40,191 @@ macro_rules! p2panda_test {
             $(-> $ret:ty)?
             $body:block
     ) => {
-        $crate::test_utils::p2panda_test!(@tracing
+        #[test]
+        fn $name() {
+            $crate::test_utils::setup_tracing();
+
             $( #[$attr] )*
-            $name
-            (
+            async fn inner(
                 $( $generator_variable : $generator_type ),*
-            )
-        {
-            #[allow(unused)]
-            let ret = $crate::test_utils::p2panda_test!(@add_tokio $body);
+            ) $( -> $ret )? {
+                $body
+            }
 
-            $($crate::test_utils::p2panda_test!(@check_return $ret, ret);)?
-        });
-    };
-
-    // Entry point for non-async fn tests
-    (
-        $( #[$attr:meta] )*
-        fn $name:ident
-            (
-                $( $generator_variable:ident : $generator_type:ty ),* $(,)?
-            )
-            $(-> $ret:ty)?
-            $body:block
-    ) => {
-        $crate::test_utils::p2panda_test!(@tracing $( #[$attr] )* $name
-            (
-                $( $generator_variable : $generator_type ),*
-            )
-        {
-            #[allow(unused)]
-            let ret = { $body };
-
-            $($crate::test_utils::p2panda_test!(@check_return $ret, ret);)?
-        });
-    };
-
-
-    // utilities
-
-    // Add tokio runtime around body
-    (@add_tokio $body:block) => {
-        {
             let rt = $crate::test_utils::create_runtime(concat!("test-runtime-", stringify!($name)))
                 .expect("Could not create runtime for tests");
 
-            rt.block_on(async { $body })
+            rt.block_on(async move {
+                let _test_result = $crate::test_utils::run_async_test(inner).await;
+            });
         }
     };
 
-    // Add tracing setup around body
-    (@tracing
-        $( #[$attr:meta] )*
-        $name:ident
-        (
-                $( $generator_variable:ident : $generator_type:ty ),* $(,)?
-        )
-        $body:block
+    // Entry point for non-async fn tests
+    //(
+    //    $( #[$attr:meta] )*
+    //    fn $name:ident
+    //        (
+    //            $( $generator_variable:ident : $generator_type:ty ),* $(,)?
+    //        )
+    //        $(-> $ret:ty)?
+    //        $body:block
+    //) => {
+    //    $crate::test_utils::p2panda_test!(@tracing $( #[$attr] )* $name
+    //        (
+    //            $( $generator_variable : $generator_type ),*
+    //        )
+    //    {
+    //        #[allow(unused)]
+    //        let ret = { $body };
+
+    //        $($crate::test_utils::p2panda_test!(@check_return $ret, ret);)?
+    //    });
+    //};
+
+
+    //// utilities
+
+    //// Add tokio runtime around body
+    //(@add_tokio $body:block) => {
+    //    {
+    //        let rt = $crate::test_utils::create_runtime(concat!("test-runtime-", stringify!($name)))
+    //            .expect("Could not create runtime for tests");
+
+    //        rt.block_on(async { $body })
+    //    }
+    //};
+
+    //// Add tracing setup around body
+    //(@tracing
+    //    $( #[$attr:meta] )*
+    //    $name:ident
+    //    (
+    //            $( $generator_variable:ident : $generator_type:ty ),* $(,)?
+    //    )
+    //    $body:block
+    //) => {
+    //    $crate::test_utils::p2panda_test!(@function
+    //        $( #[$attr] )*
+    //        $name
+    //        (
+    //            $( $generator_variable : $generator_type ),*
+    //        )
+
+    //        {
+    //            $crate::test_utils::setup_tracing();
+
+    //            $body
+    //        }
+    //    );
+    //};
+
+    //// Check the return value of the body
+    //(@check_return $ret:ty, $value:ident) => {
+    //    let ret: $ret = $value;
+    //    let ret: Result<_, _> = ret;
+
+    //    if let Err(error) = ret {
+    //        panic!("The test failed: {error:#?}");
+    //    }
+    //};
+
+    //(@function
+    //    $( #[$attr:meta] )*
+    //    $name:ident
+    //    (
+    //            $( $generator_variable:ident : $generator_type:ty ),* $(,)?
+    //    )
+    //    $body:block
+    //) => {
+    //    #[test]
+    //    $( #[$attr] )*
+    //    fn $name() {
+    //        $(
+    //            let $generator_variable = <$generator_type as $crate::test_utils::Generatable>::generate();
+    //        )*
+
+    //        $body
+    //    }
+    //}
+}
+pub use p2panda_test;
+
+pub async fn run_async_test<F, R>(testfunction: F) -> R
+where
+    F: TestFunc<Return = R>,
+    R: TestResult,
+{
+    testfunction.call_test_function().await
+}
+
+pub trait TestFunc {
+    type Return: TestResult;
+    async fn call_test_function(&self) -> Self::Return;
+}
+
+#[rustfmt::skip]
+macro_rules! all_the_tuples {
+    ($name:ident) => {
+        $name!([], T1);
+        $name!([T1], T2);
+        $name!([T1, T2], T3);
+        $name!([T1, T2, T3], T4);
+        $name!([T1, T2, T3, T4], T5);
+        $name!([T1, T2, T3, T4, T5], T6);
+        $name!([T1, T2, T3, T4, T5, T6], T7);
+        $name!([T1, T2, T3, T4, T5, T6, T7], T8);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8], T9);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9], T10);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13], T14);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14], T15);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15], T16);
+    };
+}
+
+pub trait Generate
+where
+    Self: Sized + Send,
+{
+    async fn generate() -> Self;
+}
+
+macro_rules! impl_testfunc {
+    (
+        [$($ty:ident),*], $last:ident
     ) => {
-        $crate::test_utils::p2panda_test!(@function
-            $( #[$attr] )*
-            $name
-            (
-                $( $generator_variable : $generator_type ),*
-            )
+        impl<$($ty,)* $last> TestFunc for ($($ty,)* $last,)
+        where
+            $( $ty: Generate, )*
+            $last: Generate,
+        {
+            type Return = Box<dyn TestResult>;
 
-            {
-                $crate::test_utils::setup_tracing();
+            async fn call_test_function(&self) -> Result<(), Self::Return> {
+                (self)(
+                    $(
+                        $ty::generate(),
+                    )*
 
-                $body
+                    $last::generate(),
+                ).map_err(Box::new)
             }
-        );
-    };
-
-    // Check the return value of the body
-    (@check_return $ret:ty, $value:ident) => {
-        let ret: $ret = $value;
-        let ret: Result<_, _> = ret;
-
-        if let Err(error) = ret {
-            panic!("The test failed: {error:#?}");
-        }
-    };
-
-    (@function
-        $( #[$attr:meta] )*
-        $name:ident
-        (
-                $( $generator_variable:ident : $generator_type:ty ),* $(,)?
-        )
-        $body:block
-    ) => {
-        #[test]
-        $( #[$attr] )*
-        fn $name() {
-            $(
-                let $generator_variable = <$generator_type as $crate::test_utils::Generatable>::generate();
-            )*
-
-            $body
         }
     }
 }
-pub use p2panda_test;
+
+all_the_tuples!(impl_testfunc);
+
+pub trait TestResult {}
+
+impl TestResult for () {}
+
+impl<E> TestResult for std::result::Result<(), E> where E: std::error::Error {}
+
+impl<T: TestResult + 'static> TestResult for Box<T> {}
 
 // Use the
 //
@@ -256,8 +338,8 @@ mod tests {
     use crate::Header;
     use crate::cbor::{decode_cbor, encode_cbor};
 
-    use super::*;
     use super::TestLog;
+    use super::*;
 
     #[test]
     fn zero_byte_body() {
@@ -267,19 +349,19 @@ mod tests {
         assert!(decode_cbor::<Header, _>(&bytes[..]).is_ok());
     }
 
-    #[apply(p2panda_test)]
-    fn verify_non_async_test() {}
+    // #[apply(p2panda_test)]
+    // fn verify_non_async_test() {}
 
     #[apply(p2panda_test)]
     async fn verify_async_test() {}
 
-    #[apply(p2panda_test)]
-    fn verify_extracting_random_topic(topic: RandomTopic) {
-        let _ = topic;
-    }
+    // #[apply(p2panda_test)]
+    // fn verify_extracting_random_topic(topic: RandomTopic) {
+    //     let _ = topic;
+    // }
 
-    #[apply(p2panda_test)]
-    async fn verify_async_extracting_random_topic(topic: RandomTopic) {
-        let _ = topic;
-    }
+    // #[apply(p2panda_test)]
+    // async fn verify_async_extracting_random_topic(topic: RandomTopic) {
+    //     let _ = topic;
+    // }
 }
